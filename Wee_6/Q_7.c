94bbdf575fe1d7ef4d5831d424d6038564ea286a
Sbilalahmad <syedbilalahmad397@gmail.com>
2025-08-28 08:02:09 +0530

    Wee_6 Q_4 and Q_5 done

---
+++ Wee_6/Q_7.c
@@ -0,0 +1,18 @@
+#include   <stdio.h>
+#include   <stdlib.h>
+#include   <string.h>
+#include   <math.h>
+// 7. Create a C program that accepts an IPv4 address and subnet mask in dotted decimal format, and outputs:
+//     a. Network ID
+//     b. Broadcast Address
+//     c. First and Last Host Address
+//     d. CIDR notation
+//     e. Number of valid hosts
+//     f. IP Address Class (A/B/C/D/E).
+int main()
+{
+    printf("Enter IP address: ");
+    char ip[16];
+    scanf("%s", ip);
+    return 0;
+}
